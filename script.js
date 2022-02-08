bf5da246d5f5d482d4e9e3c83c1e37b907d4a3ac
Carneiro <paulacarneiro215@gmail.com>
2022-02-08 18:21:54 +0000

    Delete old/unused code

--- script.js
+++ script.js
@@ -27,8 +27,6 @@ function startNewGame() {
   gameElement.style.display = 'flex';
 }
 
-
-
 // GAME FUNCTIONS
 
 function flipCard(card) {
@@ -38,8 +36,6 @@ function flipCard(card) {
 
 function resetCards() {
   [firstCard, secondCard] = [null, null];
-  // console.log(firstCard);
-  // console.log(secondCard);
 };
 
 function unflipCards() {
@@ -52,7 +48,6 @@ function unflipCards() {
 
 function gameOver() {
   console.log('game over');
-  // resetCards();
   setTimeout(() => {
     gameElement.style.display= 'none';
     homeElement.style.display = 'flex';
@@ -81,13 +76,11 @@ function checkForMatch() {
 function handleClick() {
   if (!firstCard) {
     firstCard = this;
-    // console.log(firstCard);
     flipCard(this);
     return ;
   }
   if (!secondCard && firstCard != this) {
     secondCard = this;
-    // console.log(secondCard);
     flipCard(this);
     checkForMatch();
   }
@@ -99,88 +92,3 @@ function shuffleCards() {
     card.style.order = position;
   });
 };
-
-// cards.forEach(card => {
-//   card.addEventListener('click', handleClick);
-// });
-
-
-// END GAME FUNCTIONS 
-
-// export const {Game} = {
-//   [firstCard, secondCard]: [null,null],
-//   match : 0,
-//   lastMatch : 10,
-  
-//   cards : document.querySelectorAll('.card'),
-
-//   flipCard(card) {
-//     card.classList.add('flipped');
-//     card.removeEventListener('click', handleClick);
-//   },
-
-//   resetCards() {
-//     [firstCard, secondCard] = [null, null];
-//     // console.log(firstCard);
-//     // console.log(secondCard);
-//   }, 
-
-//   unflipCards() {
-//     firstCard.classList.remove('flipped');
-//     secondCard.classList.remove('flipped');
-//     firstCard.addEventListener('click', handleClick);
-//     secondCard.addEventListener('click', handleClick);
-//     resetCards();
-//   },
-
-//   gameOver() {
-//     console.log('go');
-//     resetCards();
-    
-//   },
-
-//   checkForMatch() {
-//     if (firstCard.dataset.character !== secondCard.dataset.character) {
-//       setTimeout(() => {
-//         unflipCards();
-//       }, 1500);
-//       return;
-//     }
-//     match++;
-//     if (match === 10) {
-//       gameOver();
-//       return;
-//     }
-    
-//     resetCards();
-    
-//   },
-
-//   handleClick() {
-//     if (!firstCard) {
-//       firstCard = this;
-//       // console.log(firstCard);
-//       flipCard(this);
-//       return ;
-//     }
-//     if (!secondCard && firstCard != this) {
-//       secondCard = this;
-//       // console.log(secondCard);
-//       flipCard(this);
-//       checkForMatch();
-//     }
-//   },
-
-//   shuffleCards() {
-//     cards.forEach(card => {
-//       const position = Math.floor(Math.random() * 20);
-//       card.style.order = position;
-//     });
-//   },
-
-//   addEventListeners() {
-//     cards.forEach(card => {
-//       card.addEventListener('click', handleClick);
-//     })
-//   }
-// }
